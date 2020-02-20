ad005b919154d4ee1566878f430b851db49f38d0
iOS TNK <hfcb@hfcbs-iMac.local>
2020-02-20 10:21:50 +0700

    Roadmap and localisation

--- Podfile
+++ Podfile
@@ -6,17 +6,17 @@ target 'ECGame' do
   use_frameworks!
 
   # Pods for ECGame
-    pod 'MBProgressHUD'
-    pod 'IQKeyboardManagerSwift'
+    pod 'MBProgressHUD', '1.1.0'
+    pod 'IQKeyboardManagerSwift', '6.5.4'
     pod 'Toast-Swift', '~> 4.0.0'
     pod 'SwiftyJSON', '~> 4.0'  
-    pod 'Kingfisher'
+    pod 'Kingfisher', '5.11.0'
     pod 'Alamofire', '4.9.1'
     pod "SimpleAnimation"    
     pod 'ObjectMapper'
     pod 'DropDown', '2.3.4'
     pod 'AMPopTip'
-   pod 'Charts'
+    pod 'Charts'
   target 'ECGameTests' do
     inherit! :search_paths
     # Pods for testing
--- Pods/Kingfisher/Sources/General/KingfisherManager.swift
+++ Pods/Kingfisher/Sources/General/KingfisherManager.swift
@@ -125,7 +125,7 @@ public class KingfisherManager {
     /// Gets an image from a given resource.
     /// - Parameters:
     ///   - resource: The `Resource` object defines data information like key or URL.
-    ///   - options: Options to use when creating the image.
+    ///   - options: Options to use when creating the animated image.
     ///   - progressBlock: Called when the image downloading progress gets updated. If the response does not contain an
     ///                    `expectedContentLength`, this block will not be called. `progressBlock` is always called in
     ///                    main queue.
@@ -150,8 +150,9 @@ public class KingfisherManager {
         downloadTaskUpdated: DownloadTaskUpdatedBlock? = nil,
         completionHandler: ((Result<RetrieveImageResult, KingfisherError>) -> Void)?) -> DownloadTask?
     {
+        let source = Source.network(resource)
         return retrieveImage(
-            with: resource.convertToSource(),
+            with: source,
             options: options,
             progressBlock: progressBlock,
             downloadTaskUpdated: downloadTaskUpdated,
@@ -163,7 +164,7 @@ public class KingfisherManager {
     ///
     /// - Parameters:
     ///   - source: The `Source` object defines data information from network or a data provider.
-    ///   - options: Options to use when creating the image.
+    ///   - options: Options to use when creating the animated image.
     ///   - progressBlock: Called when the image downloading progress gets updated. If the response does not contain an
     ///                    `expectedContentLength`, this block will not be called. `progressBlock` is always called in
     ///                    main queue.
